e16235e2c1433aa1b1df140e26331ada85804375
Lu Quang Minh <luminh2004@gmail.com>
2022-10-06 17:58:24 +0700

    day 11

--- app.js
+++ app.js
@@ -1,63 +1,62 @@
-var successBtn = document.querySelector('.controls .success')
-var warningBtn = document.querySelector('.controls .warning')
-var errorBtn = document.querySelector('.controls .error')
+var btnSuccess = document.querySelector('.controls .success')
+var btnWarning = document.querySelector('.controls .warning')
+var btnError = document.querySelector('.controls .error')
 
-successBtn.addEventListener('click',function() {
-    createToast('success', 10000)
-})
-warningBtn.addEventListener('click',function() {
-    createToast('warning')
-})
-errorBtn.addEventListener('click',function() {
-    createToast('error')
+btnSuccess.addEventListener('click', function() {
+    showToast('success')
 })
 
-createToast('success')
+btnWarning.addEventListener('click',function() {
+    showToast('warning')
+})
+
+btnError.addEventListener('click',function() {
+    showToast('error')
+})
 
 
-function createToast(status,timeout) {
-    let templateInner = `
-                    <i class="fa-solid fa-circle-check"></i>
-                    <span class="message">This is a Success message!</span>
-                    <span class="countdown"></span>`
-    switch (status) {
+
+function showToast(status,timeout) {
+    let template = `<i class="fa-solid fa-circle-check"></i>
+    <span class="message">This is a Success message!</span>
+    <span class="countdown"></span>`
+    switch(status) {
         case 'success':
-            templateInner = `
-                    <i class="fa-solid fa-circle-check"></i>
-                    <span class="message">This is a Success message!</span>
-                    <span class="countdown"></span>`
+            template = `
+            <i class="fa-solid fa-circle-check"></i>
+            <span class="message">This is a Success message!</span>
+            <span class="countdown"></span>`
             break;
         case 'warning':
-            templateInner = `
-                    <i class="fa-solid fa-triangle-exclamation"></i>
-                    <span class="message">This is a Warning message!</span>
-                    <span class="countdown"></span>`
+            template = `
+            <i class="fa-solid fa-triangle-exclamation"></i>
+            <span class="message">This is a Warning message!</span>
+            <span class="countdown"></span>
+            `
             break;
         case 'error':
-            templateInner = `
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <span class="message">This is a Warning message!</span>
-                    <span class="countdown"></span>`
+            template = `
+            <i class="fa-solid fa-circle-exclamation"></i>
+            <span class="message">This is an Error message!</span>
+            <span class="countdown"></span>
+            `
             break;
     }
 
+
+    timeout = timeout || 5000
+    
     var toast = document.createElement('div')
     toast.classList.add('toast')
     toast.classList.add(status)
-    toast.innerHTML = `${templateInner}`
-    var totalList = document.getElementById('toasts')
-    totalList.appendChild(toast)
-    timeout = timeout || 4000
-    setTimeout(function(){
-    toast.style.animation = `slide_hide 2s ease`;
-    },timeout)
-    setTimeout(function(){
+    toast.innerHTML = `${template}`
+    var toasts = document.getElementById('toasts')
+    toasts.appendChild(toast)
+    setTimeout(function() {
+        toast.style.animation = 'slide_hide 4s ease'
+    }, timeout)
+    setTimeout(function() {
         toast.remove()
-    },timeout + 2000)
+    },timeout + 4000)
+
 }
-
-
-
-
-
-        
